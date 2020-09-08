d0443a55b9cef85eb05f21561bebfc3b03478e7a
gabihirashima <57654394+gabihirashima@users.noreply.github.com>
2020-09-08 00:16:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -19,5 +19,22 @@ int main (){
     insereElemento(getListaRadios(listaCidade), elemento);
     elemento = criaRadio("rb1.3", 5.3, 15.4, "azul", "amarelo");
     insereElemento(getListaRadios(listaCidade), elemento);
+    elemento = criaRadio("rb1.4", 45.3, 15.4, "verde", "amarelo");
+    insereElemento(getListaRadios(listaCidade), elemento);
+
+    listaStruct list = getListaRadios(listaCidade);
+    Node node = getFirst(list);
+    tipo aux;
+        while(node != NULL){
+            aux = getElemento(node);
+                if(strcmp(getId(aux), "rb1.3") == 0 ){
+                    
+                    removeElemento(getListaRadios(listaCidade), getElemento(aux));
+
+                }
+            node = getNext(node);
+        }
+
+    liberaLista(getListaRadios);
     
 }
